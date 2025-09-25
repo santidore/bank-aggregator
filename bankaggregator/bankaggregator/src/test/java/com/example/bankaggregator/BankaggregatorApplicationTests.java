bb577ffee62e7f4aae3401dda747bf2d7ead547d
Santiago Dore <santidore@hotmail.com>
2025-09-25 19:29:33 +0200

    Initial commit

---
+++ bankaggregator/bankaggregator/src/test/java/com/example/bankaggregator/BankaggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankaggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
